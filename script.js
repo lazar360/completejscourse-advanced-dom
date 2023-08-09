6414f6db0c217cd57963a062ced33b6c4ab0fa1c
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-08-09 14:47:03 +0200

    maj stringhelperparameterizedtest

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 'use strict';
 
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
+const modal = document.querySelector('.modal');
+const nav = document.querySelector('.nav');
+const overlay = document.querySelector('.overlay');
 const section1 = document.querySelector('#section--1');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
 
 ///////////////////////////////////////
 // Modal window
@@ -58,10 +62,6 @@ btnScrollTo.addEventListener('click', function (e) {
 });
 
 ///////////////////////////////////////
-// Tabbed Component
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
 
 tabsContainer.addEventListener('click', function (e) {
   const clicked = e.target.closest('.operations__tab');
@@ -83,5 +83,27 @@ tabsContainer.addEventListener('click', function (e) {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active');
 });
+
+// Menu fade animation
+const handleHover = function (e, opacity) {
+  if(e.target.classList.contains('nav__link')){
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if(el !== link) el.style.opacity = opacity;});
+      logo.style.opacity = opacity;
+  }
+};
+
+nav.addEventListener('mouseover', function (e){
+  handleHover(e, 0.5)
+});
+
+nav.addEventListener('mouseout', function (e){
+  handleHover(e, 1.0)
+});
+
 ////////////////////////////////
 // LECTURES
